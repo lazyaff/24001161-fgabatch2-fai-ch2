e4e476890882319d92d604b0b2cc8c132917e2ef
Faizal <faizalamri15@gmail.com>
2024-06-28 20:04:59 +0700

    add flowchart and update banking_system.js

--- banking_system.js
+++ banking_system.js
@@ -1,15 +1,15 @@
-import("./bank_account.js").then((module) => {
-    // Inisialisasi objek BankAccount
-    let bankAccount = new module.BankAccount();
+import { BankAccount } from "./bank_account.js";
 
-    // Menambahkan event listener
-    document.addEventListener("click", function (event) {
-        if (event.target.matches("#tambahSaldoButton")) {
-            bankAccount.deposit();
-        } else if (event.target.matches("#kurangiSaldoButton")) {
-            bankAccount.withdraw();
-        } else if (event.target.matches("#tampilkanSaldoButton")) {
-            bankAccount.show();
-        }
-    });
+// Inisialisasi objek BankAccount
+let bankAccount = new BankAccount();
+
+// Menambahkan event listener
+document.addEventListener("click", function (event) {
+    if (event.target.matches("#tambahSaldoButton")) {
+        bankAccount.deposit();
+    } else if (event.target.matches("#kurangiSaldoButton")) {
+        bankAccount.withdraw();
+    } else if (event.target.matches("#tampilkanSaldoButton")) {
+        bankAccount.show();
+    }
 });
